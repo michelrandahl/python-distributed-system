832bebeb5d4f87c656abd3beac42fd3fe44cccb7
Michel Bøje Randahl Nielsen <michel@randahl.net>
2015-05-06 03:13:19 +0200

    Update Messages.py

--- Messages.py
+++ Messages.py
@@ -1,5 +1,6 @@
+from functools import reduce
 import time
-from typing import Dict, Any
+from typing import Dict, Any, List
 
 __author__ = 'michel'
 
@@ -9,11 +10,18 @@ class ServerInfo:
         self.ip = ip
         self.port = port
 
-    def __eq__(self, other):
-        return \
-            isinstance(other, ServerInfo) and \
-            self.ip == other.ip and \
-            self.port == other.port
+    def __eq__(self, other) -> bool:
+        return (isinstance(other, ServerInfo)
+                and self.ip == other.ip
+                and self.port == other.port)
+
+    def __hash__(self) -> int:
+        return reduce(lambda acc, x: acc * 17 + x.__hash__(),
+                      [self.ip, self.port],
+                      71)
+
+    def __str__(self):
+        return self.ip + ':' + str(self.port)
 
 
 class Message:
@@ -29,7 +37,13 @@ class Inform(Message):
         self.info = info
 
 
-class SendData(Message):
+class DataMessage(Message):
     def __init__(self, data: Dict[Any, Any], sender: ServerInfo):
         super().__init__(sender)
         self.data = data
+
+
+class ServerListMessage(Message):
+    def __init__(self, servers: List[ServerInfo], sender: ServerInfo):
+        super().__init__(sender)
+        self.servers = servers
